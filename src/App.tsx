48453011e13d80c8aceb146917dfa34ba95d2649
cem-sucu <cems.cems.17@gmail.com>
2022-08-21 00:07:43 +0200

    pokédex container

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
-import React, {FunctionComponent, useState, useEffect} from 'react';
-import Pokemon from './models/pokemon';
-import POKEMONS from './models/mock-pokemon';
-  
-const App: FunctionComponent = () => {
- const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+import React, { FunctionComponent, useState, useEffect } from "react";
+import Pokemon from "./models/pokemon";
+import "./app.css";
+import POKEMONS from "./models/mock-pokemon";
 
- useEffect(() => {
-    setPokemons(POKEMONS);
- }, []);
-    
- return (
-    <div>
-        <h1 className="center">Pokédex</h1>
-        <div className="container">
-            <div className="row">
-                {pokemons.map(({id, name, picture, created}) => (
-                    <div className="col s6 m4" key={id}>
-                        <div className="card horizontal">
-                        <div className="card-image">
-                            <img src={picture} alt={name}/>
+const App: FunctionComponent = () => {
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+        setPokemons(POKEMONS);
+    }, []);
+
+    return (
+        <div>
+            <h1 className="center">Pokédex</h1>
+            <div className="container">
+                <div className="row">
+                    {pokemons.map(({ id, name, picture, created }) => (
+                        <div className="col s6 m4" key={id}>
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img src={picture} alt={name} />
+                                </div>
+                                <div className="card-stacked">
+                                    <div className="card-content">
+                                        <h6>{name}</h6>
+                                        <p>
+                                            <small>{created.toString()}</small>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="card-stacked">
-                        <div className="card-content">
-                            <p>{name}</p>
-                            <p><small>{created.toString()}</small></p>
-                        </div>
-                        </div>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
- )
-}
-  
+    );
+};
+
 export default App;
